d440341cbc348c037fe2533c601908f754caf374
kaodomi <151603952+kaodomi@users.noreply.github.com>
2024-03-21 00:27:32 +0800

    Update main1.c

--- HW#2/main1.c
+++ HW#2/main1.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
+void lotto_print(int i, int j){
+    
+}
 
 int main(){
-    printf(" 歡迎光臨長庚樂透彩購買機台\n 請問您要購買幾組樂透彩: ");
     int i=0, n;
     scanf("%d", &n);
     
+    int a[5][7];
+    
     FILE* fp;
     fp = fopen("lotto.txt", "w+");
-    time_t curtime;
-    time(&curtime);
-    fprintf(fp, "========= lotto649 =========\n");
+    fprintf(fp, "========= lotto649 =========");
     
-    srand((unsigned) time(NULL));
-    for(i=0;i<(n*7);i++){
-        if((i%7)==0){
-            fprintf(fp, "\n[%d]:",i/7+1);
+    srand(1);
+    for(i=0;i<(n*8);i++){
+        if()
+        
+        
+        if((i%8)==0){
+            fprintf(fp, "\n[%d]:",i/8+1);
         } 
         else{
             fprintf(fp, " %02d", rand()%69+1);
         }
     }
     
-    for(i=n*7;i<35;i++){
-       if((i%7)==0){
-            fprintf(fp, "\n[%d]:",i/7+1);
+    for(i=n*8;i<40;i++){
+       if((i%8)==0){
+            fprintf(fp, "\n[%d]:",i/8+1);
         } 
         else{
             fprintf(fp, " --");
@@ -34,8 +39,6 @@ int main(){
     fprintf(fp, "\n========= csie@CGU =========\n");
     
     fclose(fp);
-    
-    printf(" 已為您購買的 %d 組樂透組合輸出至 lotto.txt", n);
     
     return 0;
 }
