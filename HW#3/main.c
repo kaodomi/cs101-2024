16b62ce854727250bc74eaecd5558f7e5ba6ad4c
kaodomi <151603952+kaodomi@users.noreply.github.com>
2024-04-03 23:50:25 +0800

    Update main.c

--- HW#3/main.c
+++ HW#3/main.c
@@ -11,38 +11,6 @@ typedef struct{
     int lotto_set[5][7];
 }lotto_record_t;
 
-int main(){
-    
-    printf("歡迎光臨長庚樂透彩購買機台\n");
-    printf("請問您要買幾組樂透彩:");
-
-    int input;
-    int target[5][7] = {0};
-
-    scanf( "%d", &input);
-    
-    if(input==0){
-        printf("請輸入中獎號碼 (最多三個):");
-        int winning_num[3];
-        scanf( "%d", &winning_num[3]);
-        printf("輸入中獎號碼為:");
-        for ( int u=1 ; u<=3 ;y++){
-            printf("%02d",winning_num[u]);
-        }
-        printf("\n以下為中獎彩卷:");
-        
-    }
-    else{
-        arrayRand(target, input);
-        out2Txt(target);
-        
-        printf("已為您購買的 %d 組樂透組合輸出至 lotto.txt", input);
-    }
-
-    return 0;
-
-}
-
 
 void arrayRand(int v[5][7], int k){
 
@@ -73,13 +41,29 @@ void out2Txt(int out[5][7]){
 
     int x;
     int y;
-    int filenum=;
+    char line[256];
+    int line_count = 0;
+    int filenum = 0;
+    
+    FILE *fp_all;
+    fp_all = fopen("record.bin","a+");
+    
+    
+    while (fgets(line, sizeof(line), fp_in)) {
+        line_count++;
+        if (strstr(line, "csie@CGU")) {
+            line_count++;
+            break;
+        }
+    }
 
     FILE *fptr;
     fptr = fopen("lotto[0000"filenum"].txt","w");
     
     time_t curtime;
     time(&curtime);
+    
+    //
     
     fprintf(fptr,"======== lotto649 =========\n");
     fprintf(fptr,"=======+ No.0000%d +========", filenum);
@@ -103,6 +87,64 @@ void out2Txt(int out[5][7]){
     }
     fprintf(fptr,"======== csie@CGU =========\n");
     
+    //
+    
+    fprintf(fp_all,"======== lotto649 =========\n");
+    fprintf(fp_all,"=======+ No.0000%d +========", filenum);
+    fprintf(fp_all,"= %s", ctime(&curtime));
+    buffer[strcspn(buffer, "\n")] = '\0';
+    fprintf(fp_all," =");
+
+    for ( y=0 ; y<5 ;y++){
+        fprintf(fp_all,"[%1d]:",y+1);
+
+        for ( x=0 ; x<7 ; x++){
+            if ( out[y][x] > 0 ){
+                fprintf(fp_all," %02d",out[y][x]);
+            }
+            else{
+                fprintf(fp_all," __");
+            }
+        }
+
+        fprintf(fp_all,"\n");
+    }
+    fprintf(fp_all,"======== csie@CGU =========\n");
+    
+    fclose(fp_all);
     fclose(fptr);
 
 }
+
+int main(){
+    
+    printf("歡迎光臨長庚樂透彩購買機台\n");
+    printf("請問您要買幾組樂透彩:");
+
+    int input;
+    int target[5][7] = {0};
+
+    scanf( "%d", &input);
+    
+    if(input==0){
+        printf("請輸入中獎號碼 (最多三個):");
+        int winning_num[3];
+        scanf( "%d", &winning_num[3]);
+        printf("輸入中獎號碼為:");
+        for ( int u=1 ; u<=3 ;y++){
+            printf("%02d",winning_num[u]);
+        }
+        printf("\n以下為中獎彩卷:\n");
+        
+    }
+    else{
+        arrayRand(target, input);
+        out2Txt(target);
+        
+        printf("已為您購買的 %d 組樂透組合輸出至 lotto.txt", input);
+    }
+
+    return 0;
+
+}
+
