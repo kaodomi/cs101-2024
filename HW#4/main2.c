02717f0dd0ae20ef430d557d79e2a3893aad1401
kaodomi <151603952+kaodomi@users.noreply.github.com>
2024-04-11 15:46:01 +0800

    Update main2.c

--- HW#4/main2.c
+++ HW#4/main2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int multi(int i, int j){
+    if(j ==1){
+        printf("%d*%d=%d\n", i, j, i*j);
+        return 0;
+    } else{
+        multi(i, j-1);
+        printf("%d*%d=%d\n", i, j, i*j);
+    }
+}
+
+int main(){
+    multi(1, 9);
+    
+    return 0;
+}
 
