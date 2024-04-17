c60f5388b63bed855cb0a48f7dd01262ac23aa99
kaodomi <151603952+kaodomi@users.noreply.github.com>
2024-04-17 10:28:09 +0800

    Update main2.c

--- HW#4/main2.c
+++ HW#4/main2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int multi(int i, int j){
-    if(j ==1){
+int multiplication(int i, int j){
+    if(j ==9){
         printf("%d*%d=%d\n", i, j, i*j);
         return 0;
     } else{
-        printf("%d*%d=%d\n", i, j, i*j);
-        multi(i, j-1);
+        printf("%d*%d=%d ", i, j, i*j);
+        multiplication(i, j+1);
     }
 }
 
 int main(){
-    multi(1, 1);
+    multiplication(1, 1);
     
     return 0;
 }
